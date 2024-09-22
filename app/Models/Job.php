3dde711e774a290a82eeaa79a65d5b65590cb557
Adonat74 <donatgoninet.antoine@gmail.com>
2024-09-22 17:34:42 +0200

    eloquent relationships

--- app/Models/Job.php
+++ app/Models/Job.php
@@ -4,11 +4,20 @@ namespace App\Models;
 
 
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Job extends Model {
+
+    use HasFactory;
+
     protected $table = 'job_listing';
 
     protected $fillable = ['title', 'salary'];
+
+    public function employer()
+    {
+        return $this->belongsTo(Employer::class);
+    }
 
 }
